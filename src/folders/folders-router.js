e147d9a2891e38b7f87a556715418899adb0e544
danieljrenfro <djrenfro95@gmail.com>
2021-01-04 20:31:22 -0600

    add functionality for GET, POST, DELETE folders

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -11,9 +11,65 @@ const serializeFolder = folder => ({
   folder_name: xss(folder.folder_name)
 });
 
+foldersRouter
+  .route('/')
+  .get((req, res, next) => {
+    const db = req.app.get('db');
+    FoldersService.getAllFolders(db)
+      .then(folders => {
+        const serializedFolders = folders.map(folder => serializeFolder(folder));
+        res.json(serializedFolders);
+      });
+  })
+  .post(jsonParser, (req, res, next) => {
+    const db = req.app.get('db');
+    const { folder_name } = req.body;
+    const newFolder = { folder_name };
 
+    if (!folder_name) {
+      return res.status(400).json({
+        error: { message: `Missing 'folder_name' in request body` }
+      });
+    }
 
+    FoldersService.insertFolder(db, newFolder)
+      .then(folder => {
+        res.status(201).json(serializeFolder(folder));
+      })
+      .catch(next);
+  });
 
+foldersRouter
+  .route('/:folder_id')
+  .all((req, res, next) => {
+    const db = req.app.get('db');
+    const id = req.params.folder_id;
+
+    FoldersService.getById(db, id)
+      .then(folder => {
+        if (!folder) {
+          return res.status(404).json({
+            error: { message: `Folder doesn't exist` }
+          });
+        }
+        res.folder = folder;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.json(serializeFolder(res.folder));
+  })
+  .delete((req, res, next) => {
+    const db = req.app.get('db');
+    const id = req.params.folder_id;
+
+    FoldersService.deleteFolder(db, id)
+      .then(() => {
+        res.status(204).end();
+      })  
+      .catch(next);
+  });
 
 
 
--- test/folders-endpoints.spec.js
+++ test/folders-endpoints.spec.js
@@ -1,8 +1,9 @@
 const app = require('../src/app');
 const knex = require('knex');
-// will need my fixtures to go here
+const { makeFoldersArray } = require('../test/folders.fixtures');
+const supertest = require('supertest');
 
-describe('Folders endpoints', () => {
+describe.only('Folders endpoints', () => {
   let db;
 
   before('create knex Instance', () => {
@@ -11,5 +12,135 @@ describe('Folders endpoints', () => {
       connection: process.env.TEST_DB_URL
     });
     app.set('db', db);
+  });
+
+  before('clean tables', () => db.raw('TRUNCATE noteful_notes, noteful_folders RESTART IDENTITY CASCADE'));
+
+  afterEach('clean tables', () => db.raw('TRUNCATE noteful_notes, noteful_folders RESTART IDENTITY CASCADE'));
+
+  after('disconnect from db', () => db.destroy());
+
+  describe('GET /folders endpoint', () => {
+    context('Given there are folders in the database', () => {
+      const testFolders = makeFoldersArray();
+      
+      beforeEach('insert folders', () => {
+        return db
+          .insert(testFolders)
+          .into('noteful_folders');
+      });
+
+      it('responds with 200 and an array of folders', () => {
+        return supertest(app)
+          .get('/api/folders')
+          .expect(200, testFolders);
+      });
+    });
+
+    context('Given the database is empty', () => {
+      it('responds with 200 and an empty array', () => {
+        return supertest(app)
+          .get('/api/folders')
+          .expect(200, []);
+      });
+    });
+  });
+
+  describe('POST /folders endpoint', () => {
+    it('responds with 201 and the newly created folder', () => {
+      const newFolder = {
+        id: 1,
+        folder_name: 'Test Folder'
+      };
+
+      return supertest(app)
+        .post('/api/folders')
+        .send(newFolder)
+        .expect(201, newFolder);
+    });
+
+    it(`responds with 400 and 'Missing folder_name'`, () => {
+      const newFolder = {
+        id: 1
+      };
+
+      return supertest(app)
+        .post('/api/folders')
+        .send(newFolder)
+        .expect(400, {
+          error: { message:`Missing 'folder_name' in request body` }
+        });
+    });
+  });
+
+  describe('GET /folders/:folder_id', () => {
+    context('Given the database has folders', () => {
+      const testFolders = makeFoldersArray();
+      
+      beforeEach('insert folders', () => {
+        return db
+          .insert(testFolders)
+          .into('noteful_folders');
+      });
+
+      it('responds with 200 and the folder for the given id', () => {
+        const idOfFolder = 3;
+        const expectedFolder = testFolders[idOfFolder - 1];
+
+        return supertest(app)
+          .get(`/api/folders/${idOfFolder}`)
+          .expect(200, expectedFolder);
+      });
+    });
+
+    context('Given the database is empty', () => {
+      it(`responds with 404 and 'Folder doesn't exist'`, () => {
+        const idOfFolder = 3;
+        
+        return supertest(app)
+          .get(`/api/folders/${idOfFolder}`)
+          .expect(404, {
+            error: { message: `Folder doesn't exist` }
+          });
+      });
+    });
+  });
+
+  describe('DELETE /folders/:folder_id', () => {
+    context('Given the database has folders', () => {
+      const testFolders = makeFoldersArray();
+
+      beforeEach('insert folders', () => {
+        return db
+          .insert(testFolders)
+          .into('noteful_folders');
+      });
+
+      it(`responds with 204 and removes folder from database`, () => {
+        const idOfFolder = 3;
+        const expectedFolders = testFolders.filter(folder => folder.id !== idOfFolder);
+
+        return supertest(app)
+          .delete(`/api/folders/${idOfFolder}`)
+          .expect(204)
+          .then(() => {
+            return supertest(app)
+              .get('/api/folders')
+              .expect(expectedFolders);
+          });
+      });
+    });
+
+    context('Given the database has empty', () => {
+      it(`responds with 404 and 'Folder doesn't exist'`, () => {
+        const idOfFolder = 3;
+  
+        return supertest(app)
+          .delete(`/api/folders/${idOfFolder}`)
+          .expect(404, {
+            error: { message: `Folder doesn't exist` }
+          });
+      });
+    });
   });
 });
