a12e82a641b686b33bfadfcd7515422952240ff3
danieljrenfro <djrenfro95@gmail.com>
2021-01-04 18:32:06 -0600

    added GET, DELETE and POST functionality for Notes

--- src/notes/notes-router.js
+++ src/notes/notes-router.js
@@ -24,7 +24,67 @@ notesRouter
         const serializedNotes = notes.map(note => serializeNote(note));
 
         res.json(serializedNotes);
-      });
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const db = req.app.get('db');
+    const { note_name, content, folder_id } = req.body;
+    const newNote = { note_name, folder_id };
+
+    for(const [key, value] of Object.entries(newNote)) {
+      if (!value) {
+        return res.status(400).json({
+          error: { message: `Missing ${key} in request body` }
+        });
+      }
+    }
+
+    newNote.content = content;
+
+    NotesService.insertNote(db, newNote)
+      .then(note => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl + `/${note.id}`))
+          .json(serializeNote(note));
+      })
+      .catch(next);
   });
+
+notesRouter
+  .route('/:note_id')
+  .all((req, res, next) => {
+    const db = req.app.get('db');
+    const id = req.params.note_id;
+
+    NotesService.getById(db, id)
+      .then(note => {
+        if (!note) {
+          return res.status(404).json({
+            error: { message: `Note doesn't exist` }
+          });
+        }
+        res.note = note;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.json(serializeNote(res.note));
+  })
+  .delete((req, res, next) => {
+    const db = req.app.get('db');
+    const id = req.params.note_id;
+
+    NotesService.deleteNote(db, id)
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
+
+
+
 
 module.exports = notesRouter;
--- test/notes-endpoints.spec.js
+++ test/notes-endpoints.spec.js
@@ -3,6 +3,7 @@ const knex = require('knex');
 const { makeNotesArray } = require('../test/notes.fixtures');
 const { makeFoldersArray } = require('../test/folders.fixtures');
 const supertest = require('supertest');
+const { expect } = require('chai');
 
 
 describe('Notes endpoints', () => {
@@ -55,5 +56,143 @@ describe('Notes endpoints', () => {
     });
   });
 
-  
+  describe('POST /notes endpoint', () => {
+    const testFolders = makeFoldersArray();
+
+    beforeEach('insert folders', () => {
+      return db
+        .insert(testFolders)
+        .into('noteful_folders');
+    });
+    
+    it('responds with 201 and the newly created note', function() {
+      this.retries(3);
+      const newNote = {
+        id: 1,
+        note_name: 'Test Note',
+        content: 'Test Content',
+        folder_id: 1
+      };
+
+      return supertest(app)
+        .post('/api/notes')
+        .send(newNote)
+        .expect(201)
+        .expect(res => {
+          expect(res.body.id).to.eql(newNote.id);
+          expect(res.body.note_name).to.eql(newNote.note_name);
+          expect(res.body.content).to.eql(newNote.content);
+          expect(res.body.folder_id).to.eql(newNote.folder_id);
+          const expectedDate = new Date().toLocaleString();
+          const actualDate = new Date(res.body.date_modified).toLocaleString();
+          expect(actualDate).to.eql(expectedDate);
+          expect(res.headers.location).to.eql(`/api/notes/${res.body.id}`);
+        });
+    });
+
+    const requiredFields = ['note_name', 'folder_id'];
+
+    requiredFields.forEach(field => {
+      it(`respond with 400 and '${field} is missing'`, () => {
+        const newNote = {
+          id: 1,
+          note_name: 'Test Note',
+          content: 'Test Content',
+          folder_id: 1
+        };
+
+        delete newNote[field];
+
+        return supertest(app)
+          .post('/api/notes')
+          .send(newNote)
+          .expect(400, {
+            error: { message: `Missing ${field} in request body` }
+          });
+      });
+    });
+  });
+
+  describe('GET /notes/:note_id', () => {
+    context('Given the database has notes', () => {
+      const testNotes = makeNotesArray();
+      const testFolders = makeFoldersArray();
+
+      beforeEach('insert folders and notes', () => {
+        return db
+          .into('noteful_folders')
+          .insert(testFolders)
+          .then(() => {
+            return db
+              .into('noteful_notes')
+              .insert(testNotes);
+          });
+      });
+
+      it('responds with 200 and the found note', () => {
+        const noteId = 3;
+        const expectedNote = testNotes[noteId - 1];
+
+        return supertest(app)
+          .get(`/api/notes/${noteId}`)
+          .expect(200, expectedNote);
+      });
+    });
+
+    context('Given the database is empty', () => {
+      it(`responds with 404 and 'Note doesn't exist' if invalid id`, () => {
+        const noteId = 3;
+
+        return supertest(app)
+          .get(`/api/notes/${noteId}`)
+          .expect(404, {
+            error: { message: `Note doesn't exist` }
+          });
+      });
+    });
+  });
+
+  describe('DELETE /notes/:note_id', () => {
+    context('Given the database has notes', () => {
+      const testNotes = makeNotesArray();
+      const testFolders = makeFoldersArray();
+
+      beforeEach('insert folders and notes', () => {
+        return db
+          .into('noteful_folders')
+          .insert(testFolders)
+          .then(() => {
+            return db
+              .into('noteful_notes')
+              .insert(testNotes);
+          });
+      });
+
+      it(`responds with 204 and 'No Content'`, () => {
+        const idToDelete = 3;
+        const expectedNotes = testNotes.filter(note => note.id !== idToDelete);
+
+        return supertest(app)
+          .delete(`/api/notes/${idToDelete}`)
+          .expect(204)
+          .then(() => {
+            return supertest(app)
+              .get('/api/notes')
+              .expect(expectedNotes);
+          });
+      });
+    });
+
+    context('Given the database is empty', () => {
+      it(`responds with 404 and 'Note doesn't exist'`, () => {
+        const idToDelete = 3;
+
+        return supertest(app)
+          .delete(`/api/notes/${idToDelete}`)
+          .expect(404, {
+            error: { message: `Note doesn't exist` }
+          });
+      });
+    });
+  });
 });
